77b9e9859909a0616389887c6f7d95e3d7834577
shenzekun <726664809@qq.com>
2017-10-04 09:50:06 -0500

    Update event.js

--- src/js/mod/event.js
+++ src/js/mod/event.js
@@ -1,14 +1,14 @@
-/* 发布订阅模式 */
+/* 发布订阅模式（观察者模式） */
 var Event = (function () {
     var events = {};
-
+    //evt 代表事件类型，handler表示为回调函数 on 表示订阅
     function on(evt, handler) {
         events[evt] = events[evt] || [];
         events[evt].push({
             handler: handler
         });
     }
-
+    //fire 表示发布
     function fire(evt, args) {
         if (!events[evt]) {
             return;
@@ -17,7 +17,7 @@ var Event = (function () {
             events[evt][i].handler(args);
         }
     }
-
+    //off 表示取消订阅
     function off(name) {
         delete events[name];
     }
