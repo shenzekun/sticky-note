4ee9dfa48f61e300d48df93f4501eca0614cebc3
shenzekun <726664809@qq.com>
2017-10-22 16:55:36 +0800

    添加背景图片

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,13 +1,8 @@
-require('sass/index.scss');
-let Toast = require('mod/toast.js').Toast;
-let WaterFall = require('mod/waterfall.js');
-let NoteManager = require('mod/note-manager');
-let Event = require('mod/event');
+require("sass/index.scss");
+let Toast = require("mod/toast.js").Toast;
+let WaterFall = require("mod/waterfall.js");
+let NoteManager = require("mod/note-manager");
+let Event = require("mod/event");
 NoteManager.load();
-$('.add-note').on('click', function () {
-    NoteManager.add();
-})
-
-Event.on('waterfall', function () {
-    WaterFall.init($("#content"));
-})
+$(".add-note").on("click", () => NoteManager.add());
+Event.on("waterfall", () => WaterFall.init($("#content")));
